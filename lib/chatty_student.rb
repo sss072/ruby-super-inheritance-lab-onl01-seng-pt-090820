1a3e1c4f193b1067bdc569fa1eb6e48af622402b
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-20 20:28:21 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,7 @@
 class ChattyStuden < Student 
   def hello 
     super 
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch "
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end 
+  def raise_hand 
+    super.times 10 
